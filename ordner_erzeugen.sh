a1a2f5a358a83bffa31e275974342e8eeffaf940
Axel Tetzner <axel_tetzner@hotmail.com>
2024-11-26 16:30:57 +0100

    Zweiter Startversuch

---
+++ ordner_erzeugen.sh
@@ -0,0 +1,27 @@
+mkdir data
+mkdir data/raw
+mkdir data/interim
+mkdir data/processed
+
+mkdir notebooks
+mkdir notebooks/EDA.ipynb
+
+mkdir scripts
+mkdir scripts/data_preprocessing.py
+mkdir scripts/model_training.py
+
+mkdir src
+mkdir src/__init__.py
+mkdir src/features
+mkdir src/models
+
+mkdir models
+mkdir models/model_v1.pkl
+
+mkdir reports
+mkdir reports/figures
+
+touch README.md
+touch .gitignore
+touch requirements.txt
+
